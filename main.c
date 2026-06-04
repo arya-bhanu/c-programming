ddb4b4ebd6655698fceac2b53f09f42bf7e1d6e0
Putu Bhanuartha <aryacreative.dev@gmail.com>
2026-06-04 21:19:23 +0700

    feat: - add new command notes for safe compiling - learn using constants

--- main.c
+++ main.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 
-int main() {
-    printf("hello world\n");
+#define LOWER 0.0
+#define UPPER 300.0
+#define STEP 20.0
+
+int main()
+{
+    for (int i = LOWER; i <= UPPER; i += STEP)
+    {
+        float c = 5.0 / 9.0 * (i - 32);
+        printf("Celcius: %3f\tFahrenheit: %3f\n", c, i);
+    }
 }
