819094c0968cf7faadcf6680f3e5cc2560916847
Putu Bhanuartha <aryacreative.dev@gmail.com>
2026-06-07 18:41:33 +0700

    feat:  1.5.2 Character Counting

--- main.c
+++ main.c
@@ -1,24 +1,36 @@
 #include <stdio.h>
 
 int main() {
-    /* int c = getchar();*/
+    // 1.5.1 File Copying
+    /* */
+    int c = getchar();
 
     // will only printed the first char into console
-    /* putchar(c) */
+    /* putchar(c);  */
 
     // need to use 'while; to print all input
-    /* while (c != EOF) {
+    /*  while (c != EOF) {
         putchar(c);
         c = getchar();
-    }*/
+    } */
+    
 
     // more simpler form, use assignment as expressions
+    /*
     int c;
     while ((c = getchar()) != EOF)
     {
        putchar(c);
     }
 
-    printf("EOF: %d", EOF);
+    printf("EOF: %d", EOF); 
+    */
     
+    // 1.5.2 Character Counting
+    /*
+    int c;
+    long lc = 0;
+    while ((c  = getchar()) != EOF) ++lc;
+    printf("%ld\n", lc);
+    */
 }
