ec22ecfc75dc22f4d2542f32cc178ecc14def039
Putu Bhanuartha <aryacreative.dev@gmail.com>
2026-06-07 18:12:38 +0700

    feat: file copying

--- main.c
+++ main.c
@@ -1,5 +1,24 @@
 #include <stdio.h>
 
 int main() {
-    printf("hello world\n");
+    /* int c = getchar();*/
+
+    // will only printed the first char into console
+    /* putchar(c) */
+
+    // need to use 'while; to print all input
+    /* while (c != EOF) {
+        putchar(c);
+        c = getchar();
+    }*/
+
+    // more simpler form, use assignment as expressions
+    int c;
+    while ((c = getchar()) != EOF)
+    {
+       putchar(c);
+    }
+
+    printf("EOF: %d", EOF);
+    
 }
